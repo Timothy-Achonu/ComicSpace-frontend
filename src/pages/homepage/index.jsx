8d1bbdf7d0c0fcd590860f4816269677c76cc937
Timothy-Achonu <achonutimothy@gmail.com>
2023-04-14 15:41:34 +0100

    regular code maintenance

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -16,6 +16,16 @@ export default function Home() {
       </div>
       <div className={styles.headerMidSection}>
         <Navbar />
+        <div className={styles.heroTextsButtonWrapper}>
+          <p>
+          Get Lost in the Pages of Exciting Comic Adventure
+          </p>
+          <p>
+          Step into a world of action and excitement, join forces with legendary comic characters and 
+          unravel their stories and explore a multiverse of adventures, battles, and intrigues.
+          </p>
+          <button>Get Started</button>
+        </div>
       </div>
     </header>
   );
